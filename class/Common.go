fca0d2988d77cbd3df8afcef443dce9d17f26c43
matehaxor03 <matehaxor03>
2022-09-29 17:27:44 +1000

    remove unused code

--- class/Common.go
+++ class/Common.go
@@ -17,27 +17,6 @@ func CloneString(value *string) (*string) {
 	return &temp
 }
 
-func Examiner(t reflect.Type, depth int) {
-	fmt.Println(strings.Repeat("\t", depth), "Type is", t.Name(), "and kind is", t.Kind())
-	switch t.Kind() {
-	case reflect.Array, reflect.Chan, reflect.Map, reflect.Ptr, reflect.Slice:
-		fmt.Println(strings.Repeat("\t", depth+1), "Contained type:")
-		Examiner(t.Elem(), depth+1)
-	case reflect.Struct:
-		for i := 0; i < t.NumField(); i++ {
-			f := t.Field(i)
-			fmt.Println(strings.Repeat("\t", depth+1), "Field", i+1, "name is", f.Name, "type is", f.Type.Name(), "and kind is", f.Type.Kind())
-			if f.Tag != "" {
-				fmt.Println(strings.Repeat("\t", depth+2), "Tag is", f.Tag)
-				fmt.Println(strings.Repeat("\t", depth+2), "tag1 is", f.Tag.Get("tag1"), "tag2 is", f.Tag.Get("tag2"))
-			}
-		}
-	default:
-		panic(fmt.Sprintf("kind %s is not supported please implement", t.Kind()))
-	}
-}
-
-
 func FIELD_NAME_VALIDATION_FUNCTIONS() string {
 	return "validation_functions"
 }
