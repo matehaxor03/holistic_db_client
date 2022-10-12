f9b1ac06c95040d4cbbb9975dbcad0541f92fcd9
matehaxor03 <matehaxor03>
2022-10-12 11:20:16 +1100

    add table name field

--- class/Database.go
+++ class/Database.go
@@ -389,6 +389,9 @@ func NewDatabase(client *Client, database_name *string, database_create_options 
 				schema[field_name] = column_schema
 			}
 
+			table_name_schema := Map{"type":"*string", "value": &table_name}
+			schema["table_name"] = table_name_schema
+
 			if len(errors) > 0 {
 				return nil, errors
 			}
