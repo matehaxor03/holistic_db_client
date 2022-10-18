277231fef69f543d5bc07ed9cacb4d8bae40c932
matehaxor03 <matehaxor03>
2022-10-18 18:53:55 +1100

    add more debugging

--- class/Common.go
+++ class/Common.go
@@ -414,7 +414,7 @@ func ValidateData(fields Map, structType string) []error {
 
 		attribute_to_validate := "value"
 		if value_is_null && value_is_mandatory && parameter_fields.IsNil("default") {
-			fmt.Println(parameter_fields.ToJSONString())
+			fmt.Println(fields.ToJSONString())
 			if parameter_fields.IsBoolFalse("primary_key") {
 				errors = append(errors, fmt.Errorf("parameter: %s is mandatory but primary key is nil and default is nil", parameter))
 				continue
