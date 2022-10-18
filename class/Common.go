107f746ffefcd179701e282fe4d714c281319c5e
matehaxor03 <matehaxor03>
2022-10-18 18:21:30 +1100

    add better error messaging

--- class/Common.go
+++ class/Common.go
@@ -416,7 +416,7 @@ func ValidateData(fields Map, structType string) []error {
 		if value_is_null && value_is_mandatory && parameter_fields.IsNil("default") {
 
 			if parameter_fields.IsBoolFalse("primary_key") {
-				errors = append(errors, fmt.Errorf("parameter: %s is mandatory but was nil and had no default value", parameter))
+				errors = append(errors, fmt.Errorf("parameter: %s is mandatory but primary key is nil and default is nil", parameter))
 				continue
 			} else if parameter_fields.IsBoolTrue("primary_key") {
 				primary_key_value, primary_key_value_errors := parameter_fields.GetBool("primary_key")
@@ -424,20 +424,20 @@ func ValidateData(fields Map, structType string) []error {
 					errors = append(errors, primary_key_value_errors...)
 					continue
 				} else if *primary_key_value == false {
-					errors = append(errors, fmt.Errorf("parameter: %s is mandatory but was nil and had no default value", parameter))
+					errors = append(errors, fmt.Errorf("parameter: %s is mandatory has primary key but it was false default is nil", parameter))
 					continue
 				}
 			}
 
 			if parameter_fields.IsBoolFalse("auto_increment") {
-				errors = append(errors, fmt.Errorf("parameter: %s is mandatory but was nil and had no default value", parameter))
+				errors = append(errors, fmt.Errorf("parameter: %s is mandatory but auto_increment was nil and default is nil", parameter))
 			} else if parameter_fields.IsBoolTrue("auto_increment") {
 				auto_increment_value, auto_increment_value_errors := parameter_fields.GetBool("auto_increment")
 				if auto_increment_value_errors != nil {
 					errors = append(errors, auto_increment_value_errors...)
 					continue
 				} else if *auto_increment_value == false {
-					errors = append(errors, fmt.Errorf("parameter: %s is mandatory but was nil and had no default value", parameter))
+					errors = append(errors, fmt.Errorf("parameter: %s is mandatory but auto_increment was false and default is nil", parameter))
 					continue
 				}
 			}
