c97e61fbce6221fcb6645c04515fa75b32363dd7
matehaxor03 <matehaxor03>
2022-10-19 08:37:42 +1100

    fix logic error for checking filter column exists

--- class/Table.go
+++ class/Table.go
@@ -389,7 +389,7 @@ func NewTable(database *Database, table_name string, schema Map) (*Table, []erro
 				table_columns := getTableColumns()
 				filter_columns := filters.Keys()
 				for _, filter_column := range filter_columns {
-					if Contains(table_columns, filter_column) {
+					if !Contains(table_columns, filter_column) {
 						errors = append(errors, fmt.Errorf("SelectRecords: column: %s not found for table: %s available columns are: %s", filter_column, *getTableName(), table_columns))
 					}
 				}
