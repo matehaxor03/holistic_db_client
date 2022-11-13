3874d22d99ad0d47413743c000a34cd6c381cedd
matehaxor03 <matehaxor03>
2022-11-14 06:38:34 +1100

    refactor bool tests and add spaces between keys and values

--- tests/JSON_Bool_test.go
+++ tests/JSON_Bool_test.go
@@ -2,7 +2,6 @@ package tests
  
 import (
     "testing"
-	"fmt"
 	class "github.com/matehaxor03/holistic_db_client/class"
 )
 
@@ -11,81 +10,15 @@ func TestCanParseBoolTrue(t *testing.T) {
 
 	if json_errors != nil {
 		t.Errorf("%s", json_errors)
-	}
-	
-	has_key := json.HasKey("key")
-	if !has_key {
-		t.Errorf("key not found")
-	}
-
-	type_of := json.GetType("key")
-	if type_of != "*bool" {
-		t.Errorf("key is not a bool: %s", type_of)
-	}
-
-	value, value_errors := json.GetBool("key") 
-
-	if value_errors != nil {
-		t.Errorf("map GetBool has errors")
-	} else if value == nil {
-		t.Errorf("GetBool is nil")
-	} else if *value != true {
-		t.Errorf("expected: value actual: %t", *value)
-	}
-
-	if json != nil {
-		json_string, _ := json.ToJSONString()
-		fmt.Println(*json_string)
-	}
-}
-
-func TestCanParseBoolFalse(t *testing.T) {
-	json, json_errors := class.ParseJSON("{\"key\":false}")
-
-	if json_errors != nil {
-		t.Errorf("%s", json_errors)
-	}
-	
-	has_key := json.HasKey("key")
-	if !has_key {
-		t.Errorf("key not found")
-	}
-
-	type_of := json.GetType("key")
-	if type_of != "*bool" {
-		t.Errorf("key is not a bool: %s", type_of)
-	}
-
-	value, value_errors := json.GetBool("key") 
-
-	if value_errors != nil {
-		t.Errorf("map GetBool has errors")
-	} else if value == nil {
-		t.Errorf("GetBool is nil")
-	} else if *value != false {
-		t.Errorf("expected: value actual: %t", *value)
-	}
-
-	if json != nil {
-		json_string, _ := json.ToJSONString()
-		fmt.Println(*json_string)
-	}
-}
-
-func TestCanParseMultipleBools(t *testing.T) {
-	json, json_errors := class.ParseJSON("{\"key\":true,\"key2\":false}")
-
-	if json_errors != nil {
-		t.Errorf("%s", json_errors)
-	}
-	
-	has_key := json.HasKey("key")
-	if !has_key {
-		t.Errorf("key not found")
+	} else if json == nil {
+		t.Errorf("json is nil")
 	} else {
-		type_of := json.GetType("key")
-		if type_of != "*bool" {
-			t.Errorf("key is not a bool: %s", type_of)
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
 		} else {
 			value, value_errors := json.GetBool("key") 
 
@@ -98,14 +31,66 @@ func TestCanParseMultipleBools(t *testing.T) {
 			}
 		}
 	}
+}
 
-	has_key2 := json.HasKey("key2")
-	if !has_key2 {
-		t.Errorf("key2 not found")
+func TestCanParseBoolFalse(t *testing.T) {
+	json, json_errors := class.ParseJSON("{\"key\":false}")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json == nil {
+		t.Errorf("json is nil")
 	} else {
-		type_of := json.GetType("key2")
-		if type_of != "*bool" {
-			t.Errorf("key2 is not a bool: %s", type_of)
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetBool("key") 
+
+			if value_errors != nil {
+				t.Errorf("map GetBool has errors")
+			} else if value == nil {
+				t.Errorf("GetBool is nil")
+			} else if *value != false {
+				t.Errorf("expected: value actual: %t", *value)
+			}
+		}
+	}
+}
+
+func TestCanParseMultipleBools(t *testing.T) {
+	json, json_errors := class.ParseJSON("{\"key\":true,\"key2\":false}")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json == nil {
+		t.Errorf("json is nil")
+	} else {
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetBool("key") 
+
+			if value_errors != nil {
+				t.Errorf("map GetBool has errors")
+			} else if value == nil {
+				t.Errorf("GetBool is nil")
+			} else if *value != true {
+				t.Errorf("expected: value actual: %t", *value)
+			}
+		}
+
+		if !json.HasKey("key2") {
+			t.Errorf("key2 not found")
+		} else if json.GetType("key2") != "*bool" {
+			t.Errorf("key2 is not a *bool: %s", json.GetType("key2"))
 		} else {
 			value, value_errors := json.GetBool("key2") 
 
@@ -114,14 +99,345 @@ func TestCanParseMultipleBools(t *testing.T) {
 			} else if value == nil {
 				t.Errorf("GetBool is nil")
 			} else if *value != false {
-				t.Errorf("expected: value2 actual: %t", *value)
+				t.Errorf("expected: value actual: %t", *value)
 			}
 		}
 	}
+}
 
-	if json != nil {
-		json_string, _ := json.ToJSONString()
-		fmt.Println(*json_string)
+func TestCanParseBoolSpaceBeforeKey(t *testing.T) {
+	json, json_errors := class.ParseJSON("{ \"key\":true}")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json == nil {
+		t.Errorf("json is nil")
+	} else {
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetBool("key") 
+
+			if value_errors != nil {
+				t.Errorf("map GetBool has errors")
+			} else if value == nil {
+				t.Errorf("GetBool is nil")
+			} else if *value != true {
+				t.Errorf("expected: value actual: %t", *value)
+			}
+		}
+	}
+}
+
+func TestCanParseBoolNewlineBeforeKey(t *testing.T) {
+	json, json_errors := class.ParseJSON("{\n\"key\":true}")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json == nil {
+		t.Errorf("json is nil")
+	} else {
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetBool("key") 
+
+			if value_errors != nil {
+				t.Errorf("map GetBool has errors")
+			} else if value == nil {
+				t.Errorf("GetBool is nil")
+			} else if *value != true {
+				t.Errorf("expected: value actual: %t", *value)
+			}
+		}
+	}
+}
+
+func TestCanParseBoolDosNewlineBeforeKey(t *testing.T) {
+	json, json_errors := class.ParseJSON("{\r\n\"key\":true}")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json == nil {
+		t.Errorf("json is nil")
+	} else {
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetBool("key") 
+
+			if value_errors != nil {
+				t.Errorf("map GetBool has errors")
+			} else if value == nil {
+				t.Errorf("GetBool is nil")
+			} else if *value != true {
+				t.Errorf("expected: value actual: %t", *value)
+			}
+		}
+	}
+}
+
+func TestCanParseBoolSpaceAfterKey(t *testing.T) {
+	json, json_errors := class.ParseJSON("{\"key\" :true}")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json == nil {
+		t.Errorf("json is nil")
+	} else {
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetBool("key") 
+
+			if value_errors != nil {
+				t.Errorf("map GetBool has errors")
+			} else if value == nil {
+				t.Errorf("GetBool is nil")
+			} else if *value != true {
+				t.Errorf("expected: value actual: %t", *value)
+			}
+		}
+	}
+}
+
+func TestCanParseBoolNewlineAfterKey(t *testing.T) {
+	json, json_errors := class.ParseJSON("{\"key\"\n:true}")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json == nil {
+		t.Errorf("json is nil")
+	} else {
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetBool("key") 
+
+			if value_errors != nil {
+				t.Errorf("map GetBool has errors")
+			} else if value == nil {
+				t.Errorf("GetBool is nil")
+			} else if *value != true {
+				t.Errorf("expected: value actual: %t", *value)
+			}
+		}
+	}
+}
+
+func TestCanParseBoolDosNewlineAfterKey(t *testing.T) {
+	json, json_errors := class.ParseJSON("{\"key\"\r\n:true}")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json == nil {
+		t.Errorf("json is nil")
+	} else {
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetBool("key") 
+
+			if value_errors != nil {
+				t.Errorf("map GetBool has errors")
+			} else if value == nil {
+				t.Errorf("GetBool is nil")
+			} else if *value != true {
+				t.Errorf("expected: value actual: %t", *value)
+			}
+		}
+	}
+}
+
+func TestCanParseBoolSpaceBeforeValue(t *testing.T) {
+	json, json_errors := class.ParseJSON("{\"key\": true}")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json == nil {
+		t.Errorf("json is nil")
+	} else {
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetBool("key") 
+
+			if value_errors != nil {
+				t.Errorf("map GetBool has errors")
+			} else if value == nil {
+				t.Errorf("GetBool is nil")
+			} else if *value != true {
+				t.Errorf("expected: value actual: %t", *value)
+			}
+		}
+	}
+}
+
+func TestCanParseBoolSpaceNewlineBeforeValue(t *testing.T) {
+	json, json_errors := class.ParseJSON("{\"key\":\ntrue}")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json == nil {
+		t.Errorf("json is nil")
+	} else {
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetBool("key") 
+
+			if value_errors != nil {
+				t.Errorf("map GetBool has errors")
+			} else if value == nil {
+				t.Errorf("GetBool is nil")
+			} else if *value != true {
+				t.Errorf("expected: value actual: %t", *value)
+			}
+		}
+	}
+}
+
+func TestCanParseBoolSpaceDosNewlineBeforeValue(t *testing.T) {
+	json, json_errors := class.ParseJSON("{\"key\":\r\ntrue}")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json == nil {
+		t.Errorf("json is nil")
+	} else {
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetBool("key") 
+
+			if value_errors != nil {
+				t.Errorf("map GetBool has errors")
+			} else if value == nil {
+				t.Errorf("GetBool is nil")
+			} else if *value != true {
+				t.Errorf("expected: value actual: %t", *value)
+			}
+		}
+	}
+}
+
+func TestCanParseBoolSpaceAfterValue(t *testing.T) {
+	json, json_errors := class.ParseJSON("{\"key\":true }")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json == nil {
+		t.Errorf("json is nil")
+	} else {
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetBool("key") 
+
+			if value_errors != nil {
+				t.Errorf("map GetBool has errors")
+			} else if value == nil {
+				t.Errorf("GetBool is nil")
+			} else if *value != true {
+				t.Errorf("expected: value actual: %t", *value)
+			}
+		}
+	}
+}
+
+func TestCanParseBoolNewlineAfterValue(t *testing.T) {
+	json, json_errors := class.ParseJSON("{\"key\":true\n}")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json == nil {
+		t.Errorf("json is nil")
+	} else {
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetBool("key") 
+
+			if value_errors != nil {
+				t.Errorf("map GetBool has errors")
+			} else if value == nil {
+				t.Errorf("GetBool is nil")
+			} else if *value != true {
+				t.Errorf("expected: value actual: %t", *value)
+			}
+		}
+	}
+}
+
+func TestCanParseBoolDosNewlineAfterValue(t *testing.T) {
+	json, json_errors := class.ParseJSON("{\"key\":true\r\n}")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json == nil {
+		t.Errorf("json is nil")
+	} else {
+		PrintJSON(t, json)
+
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*bool" {
+			t.Errorf("key is not a *bool: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetBool("key") 
+
+			if value_errors != nil {
+				t.Errorf("map GetBool has errors")
+			} else if value == nil {
+				t.Errorf("GetBool is nil")
+			} else if *value != true {
+				t.Errorf("expected: value actual: %t", *value)
+			}
+		}
 	}
 }
 
--- tests/JSON_test.go
+++ tests/JSON_test.go
@@ -6,6 +6,21 @@ import (
 	class "github.com/matehaxor03/holistic_db_client/class"
 )
 
+func PrintJSON(t *testing.T, json *class.Map) {
+	if json == nil {
+		t.Errorf("json is nil")
+	} else {
+		json_string, json_string_errors := json.ToJSONString()
+		if json_string_errors != nil {
+			t.Errorf("%s", json_string_errors)
+		} else if json_string == nil {
+			t.Errorf("json_string is nil")
+		} else {
+			fmt.Println(*json_string)
+		}
+	}
+}
+
 func TestCanParseMinimal(t *testing.T) {
 	value, value_errors := class.ParseJSON("{}")
 
