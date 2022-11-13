09004196f4af836ffb6ee3e0b5a73336eaf6bf77
matehaxor03 <matehaxor03>
2022-11-13 13:55:32 +1100

    finish int16 negative

--- tests/JSON_Integer_test.go
+++ tests/JSON_Integer_test.go
@@ -110,3 +110,38 @@ func TestCanParseNegativeInt16_129(t *testing.T) {
 		t.Errorf("json is nil")
 	}	
 }
+
+func TestCanParseNegativeInt16_32768(t *testing.T) {
+	json, json_errors := class.ParseJSON("{\"key\":-32768}")
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+	} else if json != nil {
+		if !json.HasKey("key") {
+			t.Errorf("key not found")
+		} else if json.GetType("key") != "*int16" {
+			t.Errorf("key is not a *int16: %s", json.GetType("key"))
+		} else {
+			value, value_errors := json.GetInt16("key") 
+	
+			if value_errors != nil {
+				t.Errorf("map GetInt16 has errors")
+			} else if value == nil {
+				t.Errorf("GetInt16 is nil")
+			} else if *value != -32768 {
+				t.Errorf("expected: -32768  actual: %d", *value)
+			}
+		}
+
+		if json != nil {
+			json_string, json_string_errors := json.ToJSONString()
+			if json_string_errors != nil {
+				fmt.Println(json_string_errors)
+			} else {
+				fmt.Println(*json_string)
+			}
+		}
+	} else {
+		t.Errorf("json is nil")
+	}	
+}
