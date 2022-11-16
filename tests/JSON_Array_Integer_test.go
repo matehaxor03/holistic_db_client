1c2788363ececc1a05143829ff7c09c5b327c7eb
matehaxor03 <matehaxor03>
2022-11-17 08:49:51 +1100

    add tests for parsing uint64 highest value

--- tests/JSON_Array_Integer_test.go
+++ tests/JSON_Array_Integer_test.go
@@ -789,3 +789,265 @@ func TestCanParseArrayContainingMultipleUInt32_4294967294_4294967295(t *testing.
 		}
 	}
 }
+
+// uint64 boundary low
+func TestCanParseArrayContainingSingleUInt64_4294967296(t *testing.T) {
+	json := ParseJSONSuccessfully(t, "{\"key\":[4294967296]}")
+
+	if !json.HasKey("key") {
+		t.Errorf("key not found")
+	} else if json.GetType("key") != "*class.Array" {
+		t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
+	} else {			
+		value, value_errors := json.GetArray("key") 
+
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 1 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint64)) != 4294967296 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint64)))
+		}
+	}
+}
+
+func TestCanParseMultipleArraysContainingSingleUInt64_4294967296_4294967297(t *testing.T) {
+	json := ParseJSONSuccessfully(t, "{\"key\":[4294967296],\"key2\":[4294967297]}")
+
+	if !json.HasKey("key") {
+		t.Errorf("key not found")
+	} else if json.GetType("key") != "*class.Array" {
+		t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
+	} else {			
+		value, value_errors := json.GetArray("key") 
+
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 1 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint64)) != 4294967296 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint64)))
+		}
+	}
+
+	if !json.HasKey("key2") {
+		t.Errorf("key2 not found")
+	} else if json.GetType("key2") != "*class.Array" {
+		t.Errorf("key2 is not a *class.Array: %s", json.GetType("key2"))
+	} else {			
+		value, value_errors := json.GetArray("key2") 
+
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 1 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint64)) != 4294967297 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint64)))
+		}
+	}
+}
+
+func TestCanParseMultipleArraysContainingMultipleUInt64_4294967296_4294967297_4294967298_4294967299(t *testing.T) {
+	json := ParseJSONSuccessfully(t, "{\"key\":[4294967296,4294967297],\"key2\":[4294967298,4294967299]}")
+
+	if !json.HasKey("key") {
+		t.Errorf("key not found")
+	} else if json.GetType("key") != "*class.Array" {
+		t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
+	} else {			
+		value, value_errors := json.GetArray("key") 
+
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 2 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint64)) != 4294967296 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint64)))
+		} else if *((*value)[1].(*uint64)) != 4294967297 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[1].(*uint64)))
+		}
+	}
+
+	if !json.HasKey("key2") {
+		t.Errorf("key2 not found")
+	} else if json.GetType("key2") != "*class.Array" {
+		t.Errorf("key2 is not a *class.Array: %s", json.GetType("key2"))
+	} else {			
+		value, value_errors := json.GetArray("key2") 
+
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 2 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint64)) != 4294967298 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint64)))
+		} else if *((*value)[1].(*uint64)) != 4294967299 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[1].(*uint64)))
+		}
+	}
+}
+
+func TestCanParseArrayContainingMultipleUInt64_4294967296_4294967297(t *testing.T) {
+	json := ParseJSONSuccessfully(t, "{\"key\":[4294967296,4294967297]}")
+
+	if !json.HasKey("key") {
+		t.Errorf("key not found")
+	} else if json.GetType("key") != "*class.Array" {
+		t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
+	} else {			
+		value, value_errors := json.GetArray("key") 
+
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 2 {
+			t.Errorf("expected: length=2 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint64)) != 4294967296 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint64)))
+		} else if *((*value)[1].(*uint64)) != 4294967297 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[1].(*uint64)))
+		}
+	}
+}
+
+// uint64 boundary high
+func TestCanParseArrayContainingSingleUInt64_18446744073709551615(t *testing.T) {
+	json := ParseJSONSuccessfully(t, "{\"key\":[18446744073709551615]}")
+
+	if !json.HasKey("key") {
+		t.Errorf("key not found")
+	} else if json.GetType("key") != "*class.Array" {
+		t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
+	} else {			
+		value, value_errors := json.GetArray("key") 
+
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 1 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint64)) != 18446744073709551615 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint64)))
+		}
+	}
+}
+
+func TestCanParseMultipleArraysContainingSingleUInt64_18446744073709551614_18446744073709551615(t *testing.T) {
+	json := ParseJSONSuccessfully(t, "{\"key\":[18446744073709551614],\"key2\":[18446744073709551615]}")
+
+	if !json.HasKey("key") {
+		t.Errorf("key not found")
+	} else if json.GetType("key") != "*class.Array" {
+		t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
+	} else {			
+		value, value_errors := json.GetArray("key") 
+
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 1 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint64)) != 18446744073709551614 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint64)))
+		}
+	}
+
+	if !json.HasKey("key2") {
+		t.Errorf("key2 not found")
+	} else if json.GetType("key2") != "*class.Array" {
+		t.Errorf("key2 is not a *class.Array: %s", json.GetType("key2"))
+	} else {			
+		value, value_errors := json.GetArray("key2") 
+
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 1 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint64)) != 18446744073709551615 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint64)))
+		}
+	}
+}
+
+func TestCanParseMultipleArraysContainingMultipleUInt64_18446744073709551612_18446744073709551613_18446744073709551614_18446744073709551615(t *testing.T) {
+	json := ParseJSONSuccessfully(t, "{\"key\":[18446744073709551612,18446744073709551613],\"key2\":[18446744073709551614,18446744073709551615]}")
+
+	if !json.HasKey("key") {
+		t.Errorf("key not found")
+	} else if json.GetType("key") != "*class.Array" {
+		t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
+	} else {			
+		value, value_errors := json.GetArray("key") 
+
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 2 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint64)) != 18446744073709551612 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint64)))
+		} else if *((*value)[1].(*uint64)) != 18446744073709551613 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[1].(*uint64)))
+		}
+	}
+
+	if !json.HasKey("key2") {
+		t.Errorf("key2 not found")
+	} else if json.GetType("key2") != "*class.Array" {
+		t.Errorf("key2 is not a *class.Array: %s", json.GetType("key2"))
+	} else {			
+		value, value_errors := json.GetArray("key2") 
+
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 2 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint64)) != 18446744073709551614 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint64)))
+		} else if *((*value)[1].(*uint64)) != 18446744073709551615 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[1].(*uint64)))
+		}
+	}
+}
+
+func TestCanParseArrayContainingMultipleUInt64_18446744073709551614_18446744073709551615(t *testing.T) {
+	json := ParseJSONSuccessfully(t, "{\"key\":[18446744073709551614,18446744073709551615]}")
+
+	if !json.HasKey("key") {
+		t.Errorf("key not found")
+	} else if json.GetType("key") != "*class.Array" {
+		t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
+	} else {			
+		value, value_errors := json.GetArray("key") 
+
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 2 {
+			t.Errorf("expected: length=2 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint64)) != 18446744073709551614 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint64)))
+		} else if *((*value)[1].(*uint64)) != 18446744073709551615 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[1].(*uint64)))
+		}
+	}
+}
