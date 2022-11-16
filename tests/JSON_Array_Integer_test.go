92ce4feecaaa83547f0982147daf45314835b6ac
matehaxor03 <matehaxor03>
2022-11-16 22:06:13 +1100

    reduce parsing string boilerplate

--- tests/JSON_Array_Integer_test.go
+++ tests/JSON_Array_Integer_test.go
@@ -2,164 +2,135 @@ package tests
  
 import (
     "testing"
-	class "github.com/matehaxor03/holistic_db_client/class"
 )
 
 
 func TestCanParseArrayContainingSingleUInteger_0(t *testing.T) {
-	json, json_errors := class.ParseJSON("{\"key\":[0]}")
+	json := ParseJSONSuccessfully(t, "{\"key\":[0]}")
 
-	if json_errors != nil {
-		t.Errorf("%s", json_errors)
-	} else if json == nil {
-		t.Errorf("json is nil")
-	} else {
-		PrintJSON(t, json)
-		if !json.HasKey("key") {
-			t.Errorf("key not found")
-		} else if json.GetType("key") != "*class.Array" {
-			t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
-		} else {			
-			value, value_errors := json.GetArray("key") 
+	if !json.HasKey("key") {
+		t.Errorf("key not found")
+	} else if json.GetType("key") != "*class.Array" {
+		t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
+	} else {			
+		value, value_errors := json.GetArray("key") 
 
-			if value_errors != nil {
-				t.Errorf("%s", value_errors)
-			} else if value == nil {
-				t.Errorf("GetArray is nil")
-			} else if len(*value) != 1 {
-				t.Errorf("expected: length=1 actual: length=%d", len(*value))
-			} else if *((*value)[0].(*uint8)) != 0 {
-				t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint8)))
-			}
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 1 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint8)) != 0 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint8)))
 		}
-	}	
+	}
 }
 
 func TestCanParseMultipleArraysContainingSingleUInteger_0_1(t *testing.T) {
-	json, json_errors := class.ParseJSON("{\"key\":[0],\"key2\":[1]}")
+	json := ParseJSONSuccessfully(t, "{\"key\":[0],\"key2\":[1]}")
 
-	if json_errors != nil {
-		t.Errorf("%s", json_errors)
-	} else if json == nil {
-		t.Errorf("json is nil")
-	} else {
-		PrintJSON(t, json)
-		if !json.HasKey("key") {
-			t.Errorf("key not found")
-		} else if json.GetType("key") != "*class.Array" {
-			t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
-		} else {			
-			value, value_errors := json.GetArray("key") 
+	if !json.HasKey("key") {
+		t.Errorf("key not found")
+	} else if json.GetType("key") != "*class.Array" {
+		t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
+	} else {			
+		value, value_errors := json.GetArray("key") 
 
-			if value_errors != nil {
-				t.Errorf("%s", value_errors)
-			} else if value == nil {
-				t.Errorf("GetArray is nil")
-			} else if len(*value) != 1 {
-				t.Errorf("expected: length=1 actual: length=%d", len(*value))
-			} else if *((*value)[0].(*uint8)) != 0 {
-				t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint8)))
-			}
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 1 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint8)) != 0 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint8)))
 		}
+	}
 
-		if !json.HasKey("key2") {
-			t.Errorf("key2 not found")
-		} else if json.GetType("key2") != "*class.Array" {
-			t.Errorf("key2 is not a *class.Array: %s", json.GetType("key2"))
-		} else {			
-			value, value_errors := json.GetArray("key2") 
+	if !json.HasKey("key2") {
+		t.Errorf("key2 not found")
+	} else if json.GetType("key2") != "*class.Array" {
+		t.Errorf("key2 is not a *class.Array: %s", json.GetType("key2"))
+	} else {			
+		value, value_errors := json.GetArray("key2") 
 
-			if value_errors != nil {
-				t.Errorf("%s", value_errors)
-			} else if value == nil {
-				t.Errorf("GetArray is nil")
-			} else if len(*value) != 1 {
-				t.Errorf("expected: length=1 actual: length=%d", len(*value))
-			} else if *((*value)[0].(*uint8)) != 1 {
-				t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint8)))
-			}
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 1 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint8)) != 1 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint8)))
 		}
-	}	
+	}
 }
 
 func TestCanParseMultipleArraysContainingMultipleUInteger_0_1_2_3(t *testing.T) {
-	json, json_errors := class.ParseJSON("{\"key\":[0,1],\"key2\":[2,3]}")
+	json := ParseJSONSuccessfully(t, "{\"key\":[0,1],\"key2\":[2,3]}")
 
-	if json_errors != nil {
-		t.Errorf("%s", json_errors)
-	} else if json == nil {
-		t.Errorf("json is nil")
-	} else {
-		PrintJSON(t, json)
-		if !json.HasKey("key") {
-			t.Errorf("key not found")
-		} else if json.GetType("key") != "*class.Array" {
-			t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
-		} else {			
-			value, value_errors := json.GetArray("key") 
+	if !json.HasKey("key") {
+		t.Errorf("key not found")
+	} else if json.GetType("key") != "*class.Array" {
+		t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
+	} else {			
+		value, value_errors := json.GetArray("key") 
 
-			if value_errors != nil {
-				t.Errorf("%s", value_errors)
-			} else if value == nil {
-				t.Errorf("GetArray is nil")
-			} else if len(*value) != 2 {
-				t.Errorf("expected: length=1 actual: length=%d", len(*value))
-			} else if *((*value)[0].(*uint8)) != 0 {
-				t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint8)))
-			} else if *((*value)[1].(*uint8)) != 1 {
-				t.Errorf("expected \"value\" actual: %d", *((*value)[1].(*uint8)))
-			}
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 2 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint8)) != 0 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint8)))
+		} else if *((*value)[1].(*uint8)) != 1 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[1].(*uint8)))
 		}
+	}
 
-		if !json.HasKey("key2") {
-			t.Errorf("key2 not found")
-		} else if json.GetType("key2") != "*class.Array" {
-			t.Errorf("key2 is not a *class.Array: %s", json.GetType("key2"))
-		} else {			
-			value, value_errors := json.GetArray("key2") 
+	if !json.HasKey("key2") {
+		t.Errorf("key2 not found")
+	} else if json.GetType("key2") != "*class.Array" {
+		t.Errorf("key2 is not a *class.Array: %s", json.GetType("key2"))
+	} else {			
+		value, value_errors := json.GetArray("key2") 
 
-			if value_errors != nil {
-				t.Errorf("%s", value_errors)
-			} else if value == nil {
-				t.Errorf("GetArray is nil")
-			} else if len(*value) != 2 {
-				t.Errorf("expected: length=1 actual: length=%d", len(*value))
-			} else if *((*value)[0].(*uint8)) != 2 {
-				t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint8)))
-			} else if *((*value)[1].(*uint8)) != 3 {
-				t.Errorf("expected \"value\" actual: %d", *((*value)[1].(*uint8)))
-			}
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 2 {
+			t.Errorf("expected: length=1 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint8)) != 2 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint8)))
+		} else if *((*value)[1].(*uint8)) != 3 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[1].(*uint8)))
 		}
-	}	
+	}
 }
 
 func TestCanParseArrayContainingMultipleUInteger_0_1(t *testing.T) {
-	json, json_errors := class.ParseJSON("{\"key\":[0,1]}")
+	json := ParseJSONSuccessfully(t, "{\"key\":[0,1]}")
 
-	if json_errors != nil {
-		t.Errorf("%s", json_errors)
-	} else if json == nil {
-		t.Errorf("json is nil")
-	} else {
-		PrintJSON(t, json)
-		if !json.HasKey("key") {
-			t.Errorf("key not found")
-		} else if json.GetType("key") != "*class.Array" {
-			t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
-		} else {			
-			value, value_errors := json.GetArray("key") 
+	if !json.HasKey("key") {
+		t.Errorf("key not found")
+	} else if json.GetType("key") != "*class.Array" {
+		t.Errorf("key is not a *class.Array: %s", json.GetType("key"))
+	} else {			
+		value, value_errors := json.GetArray("key") 
 
-			if value_errors != nil {
-				t.Errorf("%s", value_errors)
-			} else if value == nil {
-				t.Errorf("GetArray is nil")
-			} else if len(*value) != 2 {
-				t.Errorf("expected: length=2 actual: length=%d", len(*value))
-			} else if *((*value)[0].(*uint8)) != 0 {
-				t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint8)))
-			} else if *((*value)[1].(*uint8)) != 1 {
-				t.Errorf("expected \"value\" actual: %d", *((*value)[1].(*uint8)))
-			}
+		if value_errors != nil {
+			t.Errorf("%s", value_errors)
+		} else if value == nil {
+			t.Errorf("GetArray is nil")
+		} else if len(*value) != 2 {
+			t.Errorf("expected: length=2 actual: length=%d", len(*value))
+		} else if *((*value)[0].(*uint8)) != 0 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[0].(*uint8)))
+		} else if *((*value)[1].(*uint8)) != 1 {
+			t.Errorf("expected \"value\" actual: %d", *((*value)[1].(*uint8)))
 		}
-	}	
+	}
 }
--- tests/JSON_test.go
+++ tests/JSON_test.go
@@ -6,15 +6,33 @@ import (
 	class "github.com/matehaxor03/holistic_db_client/class"
 )
 
+func ParseJSONSuccessfully(t *testing.T, json_string string) (*class.Map) {
+	json, json_errors := class.ParseJSON(json_string)
+
+	if json_errors != nil {
+		t.Errorf("%s", json_errors)
+		t.FailNow()
+	} else if json == nil {
+		t.Errorf("json is nil")
+		t.FailNow()
+	} else {
+		PrintJSON(t, json)
+	}
+	return json
+}
+
 func PrintJSON(t *testing.T, json *class.Map) {
 	if json == nil {
 		t.Errorf("json is nil")
+		t.FailNow()
 	} else {
 		json_string, json_string_errors := json.ToJSONString()
 		if json_string_errors != nil {
 			t.Errorf("%s", json_string_errors)
+			t.FailNow()
 		} else if json_string == nil {
 			t.Errorf("json_string is nil")
+			t.FailNow()
 		} else {
 			fmt.Println(*json_string)
 		}
