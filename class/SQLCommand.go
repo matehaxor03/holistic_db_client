f81340a25c46f1c94ef41617f64e0b6ee891460d
matehaxor03 <matehaxor03>
2022-12-25 18:42:21 +1100

    remove sql debug

--- class/SQLCommand.go
+++ class/SQLCommand.go
@@ -178,8 +178,6 @@ func newSQLCommand() (*SQLCommand, []error) {
 				return nil, errors
 			}
 
-			fmt.Println(sql)
-
 			records := json.Array{}
 
 			if shell_output == nil || strings.TrimSpace(*shell_output) == "" {
