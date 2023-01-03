1840d54fb823969bbfe68fcef41f5149142cff75
matehaxor03 <matehaxor03>
2023-01-03 13:18:54 +1100

    add in debug

--- class/SQLCommand.go
+++ class/SQLCommand.go
@@ -165,6 +165,7 @@ func newSQLCommand() (*SQLCommand, []error) {
 				return nil, errors
 			}
 
+			fmt.Println(command)
 			shell_output, bash_errors := bashCommand.ExecuteUnsafeCommand(command)
 
 			if sql_command_use_file {
