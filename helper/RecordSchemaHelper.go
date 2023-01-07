23a31e86639d101649104f1aa86a0fb23e365fb1
matehaxor03 <matehaxor03>
2023-01-07 12:04:49 +1100

    continue moving record helpers to seperate class

--- helper/RecordSchemaHelper.go
+++ helper/RecordSchemaHelper.go
@@ -3,7 +3,8 @@ package helper
 import (
 	//"fmt"
 	json "github.com/matehaxor03/holistic_json/json"
-	//common "github.com/matehaxor03/holistic_common/common"
+	//"strings"
+	common "github.com/matehaxor03/holistic_common/common"
 )
 
 func GetRecordColumns(caller string, data *json.Map) (*[]string, []error) {
@@ -12,5 +13,31 @@ func GetRecordColumns(caller string, data *json.Map) (*[]string, []error) {
 		return nil, fields_map_errors
 	}
 	columns := fields_map.GetKeys()
+	return &columns, nil
+}
+
+
+func GetRecordNonPrimaryKeyColumnsUpdate(caller string, data *json.Map, table_non_primary_key_columns []string) (*[]string, []error) {
+	record_columns, record_columns_errors := GetRecordColumns(caller, data)
+	if record_columns_errors != nil {
+		return nil, record_columns_errors
+	}
+
+	var errors []error
+	var columns []string
+	for _, record_column := range *record_columns {
+		if record_column == "created_date" {
+			continue
+		}
+
+		if common.Contains(table_non_primary_key_columns, record_column) {
+			columns = append(columns, record_column)
+		} 
+	}
+
+	if len(errors) > 0 {
+		return nil, errors
+	}
+
 	return &columns, nil
 }
