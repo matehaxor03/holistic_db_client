c46b3cb68917376890a42fa438df385e72834017
matehaxor03 <matehaxor03>
2023-01-11 21:12:57 +1100

    add for debug

--- dao/SQLCommand.go
+++ dao/SQLCommand.go
@@ -162,7 +162,7 @@ func newSQLCommand() (*SQLCommand, []error) {
 				return nil, errors
 			}
 
-			//fmt.Println(command)
+			fmt.Println(command)
 			shell_output, bash_errors := bashCommand.ExecuteUnsafeCommand(command, nil, nil)
 
 			if sql_command_use_file {
